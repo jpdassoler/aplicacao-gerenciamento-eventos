23ea92b87b19be2dfd87dddf92e7f23500da56d7
João Pedro Perdomo Dassoler <104994947+jpdassoler@users.noreply.github.com>
2024-09-13 16:37:24 -0300

    Criação do projeto de backend
    Criação do projeto de backend, com o Model da tabela Cliente

---
+++ EventManagerBackend/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventManagerBackend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //DbSet para a entidade Cliente
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ EventManagerBackend/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventManagerBackend.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Instagram { get; set; }
+    }
+}
