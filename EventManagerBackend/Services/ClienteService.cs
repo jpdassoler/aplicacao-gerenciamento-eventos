4e0b307bd88e4a0a87cb88039783f4fe69423400
João Pedro Perdomo Dassoler <104994947+jpdassoler@users.noreply.github.com>
2024-09-16 15:17:39 -0300

    Inclusão do Cliente Service

---
+++ EventManagerBackend/Services/ClienteService.cs
@@ -0,0 +1,52 @@
+﻿using EventManagerBackend.Models;
+using EventManagerBackend.Repositories;
+
+namespace EventManagerBackend.Services
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository) 
+        { 
+            _clienteRepository = clienteRepository;
+        }
+        public async Task<IEnumerable<Cliente>> GetAllClientes() 
+        {
+            return await _clienteRepository.GetAllClientes();
+        }
+        public async Task<Cliente> GetClienteById(string id) 
+        {
+            return await _clienteRepository.GetClienteById(id);
+        }
+        public async Task AddCliente(Cliente cliente)
+        {
+            if (String.IsNullOrEmpty(cliente.Usuario))
+            {
+                throw new ArgumentException("Usuário é obrigatório");
+            }
+            if (String.IsNullOrEmpty(cliente.Senha))
+            {
+                throw new ArgumentException("Senha é obrigatória");
+            }
+            if (String.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new ArgumentException("Nome é obrigatório");
+            }
+            await _clienteRepository.AddCliente(cliente);
+        }
+        public async Task UpdateCliente(Cliente cliente)
+        {
+            if (String.IsNullOrEmpty(cliente.Usuario))
+            {
+                throw new ArgumentException("Usuário é obrigatório");
+            }
+            await _clienteRepository.UpdateCliente(cliente);
+        }
+        public async Task DeleteCliente(string id)
+        {
+            await _clienteRepository.DeleteCliente(id);
+        }
+
+    }
+}
---
+++ EventManagerBackend/Services/IClienteService.cs
@@ -0,0 +1,13 @@
+﻿using EventManagerBackend.Models;
+
+namespace EventManagerBackend.Services
+{
+    public interface IClienteService
+    {
+        Task<IEnumerable<Cliente>> GetAllClientes();
+        Task<Cliente> GetClienteById(string id);
+        Task AddCliente(Cliente cliente);
+        Task UpdateCliente(Cliente cliente);
+        Task DeleteCliente(string id);
+    }
+}
