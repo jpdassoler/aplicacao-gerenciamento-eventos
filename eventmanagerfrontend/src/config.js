76f44a23c168d3a34cdbd2f3211006b541d8d6d8
João Pedro Perdomo Dassoler <104994947+jpdassoler@users.noreply.github.com>
2024-10-20 15:23:25 -0300

    Alteração na chamada backend para API

--- eventmanagerfrontend/src/config.js
+++ eventmanagerfrontend/src/config.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 let apiUrl = '';
 
 export const fetchConfig = async () => {
+    const configFileName = process.env.NODE_ENV === 'production' ? 'config.json' : 'config-dev.json';
+
     try {
-        const response = await axios.get('/config.json');
+        const response = await axios.get(`${configFileName}`);
         console.log('Config carregada:', response.data);
         apiUrl = response.data.REACT_APP_API_URL;
     } catch (error) {
